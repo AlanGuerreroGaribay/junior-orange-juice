82fa4bab88103eddc2b5b08488fdaf1d8cb4b001
Alan <mrrobotfield@gmail.com>
2024-11-12 10:42:00 -0600

    correcciones

--- src/components/FormInputs/FormInput.jsx
+++ src/components/FormInputs/FormInput.jsx
@@ -1,5 +1,23 @@
 // eslint-disable-next-line react/prop-types
 export const FormInput = ({ name, value, onChange, placeholder, error }) => {
+  const placeHolderHandler = (stateName) => {
+    let placeholder = "";
+    switch (stateName) {
+      case "Categoria":
+        placeholder = "12-14 años";
+        break;
+      case "Pais":
+        placeholder = "País (No disponible para Estados Unidos)";
+        break;
+      default:
+        placeholder = stateName;
+    }
+
+    return placeholder;
+  };
+
+  const placeholderText = placeHolderHandler(placeholder);
+
   return (
     <>
       <input
@@ -8,7 +26,7 @@ export const FormInput = ({ name, value, onChange, placeholder, error }) => {
         disabled={name === "Categoria" ? true : false}
         value={name === "Edad" && value < 0 ? 0 : value}
         onChange={onChange}
-        placeholder={placeholder === "Categoria" ? "12-14 años" : placeholder}
+        placeholder={placeholderText}
         type={name === "Edad" ? "number" : ""}
         className="input"
       />
