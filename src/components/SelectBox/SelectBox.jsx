611b068177db704d7a95d11e355d48a8cbb163f7
Diego Gonzalez <diegoaglez91@gmail.com>
2024-11-13 15:07:48 -0600

    opciones del select e imagen

--- src/components/SelectBox/SelectBox.jsx
+++ src/components/SelectBox/SelectBox.jsx
@@ -5,17 +5,17 @@ export const SelectBox = ({ name, onChange }) => {
   return (
     <select name={name} onChange={onChange} className="input">
       <option value="">-- Seleccione una categoría --</option>
-      <option value="U14 nacidos 2010 y 2011 femenil">
-        U14 nacidos 2010 y 2011 femenil{" "}
+      <option value="U14 nacidos del 2010 al 2011 femenil">
+        U14 nacidos del 2010 al 2011 femenil{" "}
       </option>
-      <option value="U14 nacidos 2010 y 2011 varonil">
-        U14 nacidos 2010 y 2011 varonil{" "}
+      <option value="U14 nacidos del 2010 al 2011 varonil">
+        U14 nacidos del 2010 al 2011 varonil{" "}
       </option>
-      <option value="U12 nacidos 2012 y 2014 femenil ">
-        U12 nacidos 2012 y 2014 femenil{" "}
+      <option value="U12 nacidos del 2012 al 2014 femenil"> 
+        U12 nacidos del 2012 al 2014 femenil{" "}
       </option>
-      <option value="U12 nacidos 2012 y 2014 varonil">
-        U12 nacidos 2012 y 2014 varonil{" "}
+      <option value="U12 nacidos del 2012 al 2014 varonil">
+        U12 nacidos del 2012 al 2014 varonil{" "}
       </option>
     </select>
   );
